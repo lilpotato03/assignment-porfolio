acc0c656905da233145ee3ab5a01314c3d7585b5
lilpotato03 <115008378+lilpotato03@users.noreply.github.com>
2024-06-15 07:30:57 +0000

    Made changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,18 +24,18 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aaron Furtado",
+  title: "Computer Engineering Student",
+  email: "aaron.furtado20003@gmail.com",
+  gitHub: "https://github.com/lilpotato03",
+  instagram: "https://www.instagram.com/aaronfurtado_/#",
+  linkedIn: "https://www.linkedin.com/in/aaron-furtado-41b739289/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
+const primaryColor = "#808080";
 const secondaryColor = "#D2F1E4";
 
 const App = () => {
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/funk-3.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a 3rd year engineering student studying at Fr. Conceicao Rodrigues College of Engineering. I enjoy creating Full Stack WebApps and Working on AI projects.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -37,6 +37,9 @@ const skillsList = [
   "Focus group testing",
   "Mobile user interfaces",
   "Graphic design",
+  "Machine learning",
+  "Competitive programming",
+  "Full stack web development"
 ];
 
 /**
